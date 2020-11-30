53ddb727beea5856cee0aabb4f79c11d6d2e03be
gaoc <273354076@qq.com>
2020-12-01 07:52:20 +0800

    shineyork/tiny-laravel init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoc
+ * Date: 2020/11/30
+ * Time: 下午2:43
+ */
+require "vendor/autoload.php";
+use ShineYork\TinyLaravel\Test;
+
+$t = new Test();
+echo $t->index();
+
