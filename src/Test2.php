c391c19542ab359f110e8fbb00381f72fa887935
gaoc <273354076@qq.com>
2020-12-01 10:51:17 +0800

    add file

---
+++ src/Test2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoc
+ * Date: 2020/11/30
+ * Time: 下午2:40
+ */
+namespace ShineYork\TinyLaravel;
+
+class Test2{
+    public function index()
+    {
+        return "hello!!!";
+    }
+}
